1643f3ce5b915c40abe5b5a269dde8abfec2d9f8
tson1 <demoncheng0@gmail.com>
2021-01-03 21:42:39 +0900

    eur/usd

--- DataImport-02-insert.sql
+++ DataImport-02-insert.sql
@@ -20,3 +20,26 @@ select
 from [dataTmp]
 order by c1
 
+
+/*
+SELECT *
+,SUBSTRING(tmstamp,1,4) as y
+,SUBSTRING(tmstamp,5,2) as mt
+,SUBSTRING(tmstamp,7,2) as d
+
+,SUBSTRING(tmstamp,10,2) as h
+
+,SUBSTRING(tmstamp,12,2) as m
+,SUBSTRING(tmstamp,14,2) as s
+  FROM [test].[dbo].[fxdata]
+  where id=1;
+
+update [fxdata]
+set y=SUBSTRING(tmstamp,1,4) 
+,mt=SUBSTRING(tmstamp,5,2) 
+,d=SUBSTRING(tmstamp,7,2) 
+,h=SUBSTRING(tmstamp,10,2) 
+,m=SUBSTRING(tmstamp,12,2) 
+,s='00'
+
+*/
